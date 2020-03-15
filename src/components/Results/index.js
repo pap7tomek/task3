db682c2fce6b08569253d31c8c62d3fa78c24443
pap7tomek <pap7tomek@gmail.com>
2020-03-15 07:39:21 +0100

    fixed display result

--- src/components/Results/index.js
+++ src/components/Results/index.js
@@ -3,10 +3,25 @@ import ResultContext from '../../context/resultContext'
 
 const Results = () => {
   const result = useContext(ResultContext);
+  const changeResultDisplay = (item) => {
+    return {
+      sections: item.sections.map((section) => {
+        return {
+          name: section.name,
+          fields: section.fields.map((field) => {
+            return {
+              name: field.name,
+              value: field.default
+            }
+          })
+        }
+      })
+    }
+  }
   return (
       <pre>
         {
-          result.result && JSON.stringify(result.result, null, 4)
+          result.result && JSON.stringify(changeResultDisplay(result.result), null, 4)
         }
       </pre>
   )
